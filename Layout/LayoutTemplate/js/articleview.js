20920d7fa0bfd40f45a9d3fd5f72ff872c0457ff
YogaViraj <98088131+SRIRAM1745@users.noreply.github.com>
2022-04-25 17:51:47 +0530

    fgd

---
+++ Layout/LayoutTemplate/js/articleview.js
@@ -0,0 +1,20 @@
+function myFunction()
+         {
+            var dots = document.getElementById("dots");
+            var moreText = document.getElementById("more");
+            var btnText = document.getElementById("myBtn");
+
+            if (dots.style.display === "none") 
+            {
+                dots.style.display = "inline";
+                btnText.innerHTML = "Click here to view full article";
+                moreText.style.display = "none";
+            } 
+            else
+            {
+                dots.style.display = "none";
+                btnText.innerHTML = "Click here to view less article";
+                moreText.style.display = "inline";
+            }
+        }
+   
